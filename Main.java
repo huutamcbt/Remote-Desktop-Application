4bad088d927a7a1a5c04d9d086bb53320f04b9dc
huutamcbt <huutamcbt@gmail.com>
2023-02-19 17:42:04 +0700

    Update README file

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
 import Views.MainFrame;
 import java.awt.*;
-
 import javax.tools.Tool;
-public class Main {
-    public static void main(String[] args){
-        MainFrame frame = new MainFrame();
-        if(frame.isDisplayable()){
 
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        if (frame.isDisplayable()) {
         }
         frame.setVisible(true);
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-   
-       
+
     }
 }
